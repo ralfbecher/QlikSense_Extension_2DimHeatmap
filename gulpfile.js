e2db77bc3195ee59508ae4a74003102aa26eb0c7
Martin Walter <martin.walter@tretton37.com>
2018-10-16 13:15:07 +0200

    [QPE-124] Upgrading to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,14 @@
 var gulp = require('gulp');
 var zip = require('gulp-zip');
 var del = require('del');
-var runSequence = require('run-sequence');
 var path = require('path');
 var buildDest = path.resolve("./build");
 
 var srcFiles = path.resolve('./src/**/*.*');
 var name = require('./package.json').name;
 
-gulp.task('remove-build-zip', function(){
-  del.sync([buildDest + "/" + name + '.zip']);
+gulp.task('remove-build-folder', function(){
+  return del([buildDest]);
 });
 
 gulp.task('zip-build', function(){
@@ -22,11 +21,10 @@ gulp.task('add-src', function(){
   return gulp.src(srcFiles).pipe(gulp.dest(buildDest));
 });
 
-gulp.task('build', function() {
-  runSequence(
-    'remove-build-zip',
-    'add-src',
-    'zip-build'
-  );
-});
+gulp.task('build',
+  gulp.series('remove-build-folder','add-src','zip-build')
+);
 
+gulp.task('default',
+  gulp.series('build')
+);
