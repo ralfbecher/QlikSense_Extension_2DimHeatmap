9922c657504ecd3d0d0303d4944a6a6fa0733073
ahmed-Bazzara <ahmed.bazzara@tretton37.com>
2018-10-26 11:11:40 +0200

    LESS configurations added

--- webpack.config.js
+++ webpack.config.js
@@ -34,8 +34,8 @@ module.exports = {
         }
       },
       {
-        test: /.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
       }
     ]
   },
